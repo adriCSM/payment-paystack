072aff90e73c9bb7fda5a051e56f5bd279b2cfbf
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-08-23 07:00:14 +0800

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
 dotenv.config();
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`server runing on port ${process.env.PORT}`);
+});
